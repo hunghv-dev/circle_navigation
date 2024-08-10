6c0514e6a2869eb72fdbe39f3c52d565a667faac
hunghv <hunghv.com@gmail.com>
2024-08-10 15:25:57 +0700

    main.dart: update sample app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:circle_navigation/circle_navigation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(home: MyApp()));
-}
+void main() => runApp(const MaterialApp(home: MyApp()));
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -13,47 +11,70 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        children: [
+  late int initIndex;
+  late PageController pageController;
 
-        ],
-      ),
-      bottomNavigationBar: CircleNavigation(
-        items: [
-          Item(
-              icon: const Icon(
-                Icons.home,
-                color: Colors.white,
-                size: 30,
-              ),
-              label: 'Home'),
-          Item(
-              icon: const Icon(
-                Icons.account_circle,
-                color: Colors.white,
-                size: 30,
-              ),
-              label: 'User'),
-          Item(
-              icon: const Icon(
-                Icons.settings,
-                color: Colors.white,
-                size: 30,
-              ),
-              label: 'Settings'),
-        ],
-        textStyle: const TextStyle(
-          color: Colors.white,
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-        circleColor: Colors.green,
-        navColor: Colors.lightBlue,
-        onChange: (_) {},
-      ),
-    );
+  @override
+  void initState() {
+    super.initState();
+    initIndex = 0;
+    pageController = PageController(initialPage: initIndex);
   }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: PageView(
+          controller: pageController,
+          physics: const NeverScrollableScrollPhysics(),
+          children: const [
+            Icon(Icons.home, color: Colors.green, size: 150),
+            Icon(Icons.account_circle, color: Colors.green, size: 150),
+            Icon(Icons.settings, color: Colors.green, size: 150),
+          ],
+        ),
+        bottomNavigationBar: CircleNavigation(
+          initIndex: initIndex,
+          items: [
+            Item(
+                icon: const Icon(
+                  Icons.home,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                label: 'Home'),
+            Item(
+                icon: const Icon(
+                  Icons.account_circle,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                label: 'User'),
+            Item(
+                icon: const Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                label: 'Settings'),
+          ],
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          circleColor: Colors.green,
+          navColor: Colors.blue,
+          onChange: (index) => pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.ease,
+          ),
+        ),
+      );
 }
